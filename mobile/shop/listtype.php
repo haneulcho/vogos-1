29769f0ef4babd04467cc7e7dbb18a0b4cea80c6
unknown <stuykr@naver.com>
2015-06-11 14:36:12 +0900

    listtype.php (new, best, md's choice 모바일 마크업 수정)

--- mobile/shop/listtype.php
+++ mobile/shop/listtype.php
@@ -3,12 +3,21 @@ include_once('./_common.php');
 
 $type = preg_replace("/[\<\>\'\"\\\'\\\"\%\=\(\)\s]/", "", $_REQUEST['type']);
 if ($type == 1)      $g5['title'] = '히트상품';
-else if ($type == 2) $g5['title'] = '추천상품';
-else if ($type == 3) $g5['title'] = '최신상품';
-else if ($type == 4) $g5['title'] = '인기상품';
+else if ($type == 2) {
+    $g5['title'] = 'MD\'S CHOICE';
+    $nbm_class = 'md_choice';
+}
+else if ($type == 3) {
+    $g5['title'] = 'NEW ARRIVALS';
+    $nbm_class = 'new_arrivals';
+}
+else if ($type == 4) {
+    $g5['title'] = 'BEST ITEMS';
+    $nbm_class = 'best_item';
+}
 else if ($type == 5) $g5['title'] = '할인상품';
 else
-    alert('상품유형이 아닙니다.');
+    alert('상품유형이 아닙니다 .');
 
 include_once(G5_MSHOP_PATH.'/_head.php');
 
@@ -17,8 +26,8 @@ include_once(G5_MSHOP_PATH.'/_head.php');
 $list_mod   = 1;    // 한줄에 이미지 몇개씩 출력? 단, 모바일환경에서는 사용되지 않음.
 $list_row   = 10;    // 한 페이지에 몇라인씩 출력?
 
-$img_width  = 230;  // 출력이미지 폭
-$img_height = 230;  // 출력이미지 높이
+$img_width  = 300;  // 출력이미지 폭
+$img_height = 420;  // 출력이미지 높이
 ?>
 
 <?php
@@ -36,6 +45,14 @@ define('G5_SHOP_CSS_URL', G5_MSHOP_SKIN_URL);
 
 // 리스트 유형별로 출력
 $list_file = G5_MSHOP_SKIN_PATH.'/'.$skin;
+
+// header title 출력
+echo '<div id="sidx">';
+if($type ==2 || $type ==3 || $type ==4){
+    $nbm = '<div class="item '.$nbm_class.'"><header><h2><a href="'.G5_MSHOP_URL.'/listtype.php?type='.$type.'">'.$g5['title'].'</a></h2></header>';
+    echo $nbm;
+}
+
 if (file_exists($list_file)) {
     // 총몇개 = 한줄에 몇개 * 몇줄
     $items = $list_mod * $list_row;
@@ -65,6 +82,7 @@ else
 {
     echo '<div align="center">'.$skin.' 파일을 찾을 수 없습니다.<br>관리자에게 알려주시면 감사하겠습니다.</div>';
 }
+echo '</div></div>';
 ?>
 
 <?php
