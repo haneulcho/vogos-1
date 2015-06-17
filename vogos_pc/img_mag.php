b8d927d9a6f411365802a80d3b0eee30d8a4c356
unknown <stuykr@naver.com>
2015-06-17 18:58:41 +0900

    pc 스크립트 파일 js 디렉토리 따로 만들어 분리

--- vogos_pc/img_mag.php
+++ vogos_pc/img_mag.php
@@ -38,7 +38,7 @@ bSrc = "<?php echo G5_DATA_URL.'/item/'.$it['it_img1']; ?>";
 bWidth = "<?php echo $size[0]; ?>";
 bHeight = "<?php echo $size[1]; ?>";
 </script>
-<script src="<?php echo G5_SHOP_SKIN_URL; ?>/img_mag.js"></script>
+<script src="<?php echo G5_SHOP_SKIN_URL; ?>/js/img_mag.js"></script>
 		<div id="sit_pvi">
             <div id="sit_pvi_img">
     			<div id="sit_pvi_small">
