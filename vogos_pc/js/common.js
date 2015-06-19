a43ddcf29579a22b143ac902d8107c53af1a1a25
unknown <stuykr@naver.com>
2015-06-19 18:00:08 +0900

    pc 스크롤-다운시 side navigation 따라오게 애니메이션 추가

--- vogos_pc/js/common.js
+++ vogos_pc/js/common.js
@@ -1,9 +1,27 @@
 // #ft_totop 클릭하면 최상단으로 이동
 $(function(){
-    $('#ft_totop').click(function(e) {
-        e.preventDefault();
-        $("html, body").animate({
-            scrollTop: 0
-        }, 600);
-    });
+	$('#ft_totop').click(function(e) {
+			e.preventDefault();
+			$("html, body").animate({
+					scrollTop: 0
+			}, 600);
+	});
+	var timer;
+	var navPosition = $('#side').offset().top;
+	$(window).scroll(function() {
+		var wPosition = $(window).scrollTop();
+		if(timer) {
+			clearTimeout(timer);
+		}
+		timer = setTimeout(function() {
+			if(navPosition < wPosition) {
+				if(!$('#side').hasClass('scrolled')){
+					$('#side').addClass('scrolled');
+				}
+			}
+			if(220 > wPosition) {
+				$('#side').removeClass('scrolled');
+			}
+		}, 100);
+	});
 });
