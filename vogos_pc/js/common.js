c4ee16901ca672bf3d4bafdbc86bdaa6348c4c1a
unknown <stuykr@naver.com>
2015-06-22 11:25:58 +0900

    pc 스크롤-다운시 side navigation 코드 예외처리

--- vogos_pc/js/common.js
+++ vogos_pc/js/common.js
@@ -1,27 +1,36 @@
-// #ft_totop 클릭하면 최상단으로 이동
 $(function(){
-	$('#ft_totop').click(function(e) {
-			e.preventDefault();
-			$("html, body").animate({
-					scrollTop: 0
-			}, 600);
-	});
+// #ft_totop 클릭하면 최상단으로 이동
+$('#ft_totop').click(function(e) {
+	e.preventDefault();
+	$("html, body").animate({
+		scrollTop: 0
+	}, 600);
+});
+// #side 스크롤 따라 네비게이션 이동
+function scrollNav(){
 	var timer;
-	var navPosition = $('#side').offset().top;
-	$(window).scroll(function() {
-		var wPosition = $(window).scrollTop();
-		if(timer) {
-			clearTimeout(timer);
-		}
-		timer = setTimeout(function() {
-			if(navPosition < wPosition) {
-				if(!$('#side').hasClass('scrolled')){
-					$('#side').addClass('scrolled');
+	var cHeight = $('body').find('#container').height();
+	var sideNav = $('body').find('#side');
+	if(cHeight < 681) {
+		sideNav.css('position', 'static');
+	} else {
+		var navPosition = sideNav.offset().top;
+		$(window).scroll(function() {
+			var wPosition = $(window).scrollTop();
+			if(timer) { clearTimeout(timer); }
+			timer = setTimeout(function() {
+				if(navPosition < wPosition) {
+					if(!sideNav.hasClass('scrolled')){
+						sideNav.addClass('scrolled');
+					}
 				}
-			}
-			if(220 > wPosition) {
-				$('#side').removeClass('scrolled');
-			}
-		}, 100);
-	});
+				if(220 > wPosition) {
+					sideNav.removeClass('scrolled');
+				}
+			}, 100);
+		}); // scroll function END
+	}
+}; // scrollNav END
+scrollNav();
+
 });
