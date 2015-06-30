a72aecfc093c43642e53ad2492d5e0a2ea24138d
unknown <stuykr@naver.com>
2015-06-30 17:03:01 +0900

    mobile footer 디자인 개선

--- mobile/shop/shop.tail.php
+++ mobile/shop/shop.tail.php
@@ -11,23 +11,23 @@ $admin = get_admin("super");
 
 <div id="ft">
     <h2><?php echo $config['cf_title']; ?></h2>
-    <a class="ftLogo" href="<?php echo $default['de_root_index_use'] ? G5_URL : G5_SHOP_URL; ?>/"><img src="<?php echo G5_DATA_URL; ?>/common/mobile_logo_img.png" alt="처음으로"></a>
+    <ul>
+        <li><a href="<?php echo G5_SHOP_URL; ?>/?device=pc" id="ft_to_pc"><i class="ion-monitor"></i>PC</a></li>
+        <li><a href="#" id="ft_totop"><i class="ion-arrow-up-c"></i>TOP</a></li>
+    </ul>
     <p>
         <span><b>회사명</b> <?php echo $default['de_admin_company_name']; ?></span>
-        <span><b>주소</b> <?php echo $default['de_admin_company_addr']; ?></span><br>
-        <span><b>사업자 등록번호</b> <?php echo $default['de_admin_company_saupja_no']; ?></span>
         <span><b>대표</b> <?php echo $default['de_admin_company_owner']; ?></span><br>
+        <span><b>주소</b> <?php echo $default['de_admin_company_addr']; ?></span><br>
         <span><b>전화</b> <?php echo $default['de_admin_company_tel']; ?></span>
         <span><b>팩스</b> <?php echo $default['de_admin_company_fax']; ?></span><br>
-        <!-- <span><b>운영자</b> <?php echo $admin['mb_name']; ?></span><br> -->
+        <span><b>사업자 등록번호</b> <?php echo $default['de_admin_company_saupja_no']; ?></span>
+        <!-- <span><b>운영자</b> <?php //echo $admin['mb_name']; ?></span><br> -->
         <span><b>통신판매업신고번호</b> <?php echo $default['de_admin_tongsin_no']; ?></span>
-        <span><b>개인정보관리책임자</b> <?php echo $default['de_admin_info_name']; ?></span>
-
-        <?php if ($default['de_admin_buga_no']) echo '<span><b>부가통신사업신고번호</b> '.$default['de_admin_buga_no'].'</span>'; ?><br>
-        Copyright &copy; 2001-2013 <?php echo $default['de_admin_company_name']; ?>. All Rights Reserved.
+        <?php if ($default['de_admin_buga_no']) echo '<span><b>부가통신사업신고번호</b> '.$default['de_admin_buga_no'].'</span>'; ?>
+        <!-- <span><b>개인정보관리책임자</b> <?php //echo $default['de_admin_info_name']; ?></span>--><br>
+        Copyright &copy; 2015 <?php echo $default['de_admin_company_name']; ?>. All Rights Reserved.
     </p>
-    <a href="<?php echo G5_SHOP_URL; ?>/?device=pc" id="ft_to_pc">PC버전</a>
-    <a href="#" id="ft_totop">상단으로</a>
 </div>
 </div> <!-- main END -->
 
