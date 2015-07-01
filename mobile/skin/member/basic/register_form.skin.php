ecc3c40c2d31d61152142d7960a218d14e974f12
unknown <stuykr@naver.com>
2015-07-01 15:00:23 +0900

    mobile 회원가입폼 최종 완료

--- mobile/skin/member/basic/register_form.skin.php
+++ mobile/skin/member/basic/register_form.skin.php
@@ -26,7 +26,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 
     <div class="tbl_frm01 tbl_wrap">
         <table>
-        <caption>기본정보 입력</caption>
+        <caption>로그인정보 입력</caption>
         <tr>
             <th scope="row"><label for="reg_mb_id">아이디<strong class="sound_only">필수</strong></label></th>
             <td>
@@ -47,7 +47,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 
     <div class="tbl_frm01 tbl_wrap">
         <table>
-        <caption>개인정보 입력</caption>
+        <caption>기본정보 입력</caption>
         <tr>
             <th scope="row"><label for="reg_mb_name">이름<strong class="sound_only">필수</strong></label></th>
             <td>
@@ -118,27 +118,29 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
             </td>
         </tr>
         <?php } ?>
+        </table>
+    </div>
 
+    <div class="tbl_frm01 tbl_wrap">
+        <table>
+        <caption>주소정보 입력</caption>
         <?php if ($config['cf_use_addr']) { ?>
         <tr>
-            <th scope="row">
-                주소
-                <?php if ($config['cf_req_addr']) { ?><strong class="sound_only">필수</strong><?php } ?>
-            </th>
-            <td>
+            <td style="padding:13px;">
                 <label for="reg_mb_zip1" class="sound_only">우편번호 앞자리<?php echo $config['cf_req_addr']?'<strong class="sound_only"> 필수</strong>':''; ?></label>
                 <input type="text" name="mb_zip1" value="<?php echo $member['mb_zip1'] ?>" id="reg_mb_zip1" <?php echo $config['cf_req_addr']?"required":""; ?> class="frm_input <?php echo $config['cf_req_addr']?"required":""; ?>" size="3" maxlength="3">
                 -
                 <label for="reg_mb_zip2" class="sound_only">우편번호 뒷자리<?php echo $config['cf_req_addr']?'<strong class="sound_only"> 필수</strong>':''; ?></label>
                 <input type="text" name="mb_zip2" value="<?php echo $member['mb_zip2'] ?>" id="reg_mb_zip2" <?php echo $config['cf_req_addr']?"required":""; ?> class="frm_input <?php echo $config['cf_req_addr']?"required":""; ?>" size="3" maxlength="3">
-                <button type="button" class="btn_frmline" onclick="win_zip('fregisterform', 'mb_zip1', 'mb_zip2', 'mb_addr1', 'mb_addr2', 'mb_addr3', 'mb_addr_jibeon');">주소 검색</button><br>
                 <label for="reg_mb_addr1" class="sound_only">주소<?php echo $config['cf_req_addr']?'<strong class="sound_only"> 필수</strong>':''; ?></label>
-                <input type="text" name="mb_addr1" value="<?php echo $member['mb_addr1'] ?>" id="reg_mb_addr1" <?php echo $config['cf_req_addr']?"required":""; ?> class="frm_input frm_address <?php echo $config['cf_req_addr']?"required":""; ?>" size="50"><br>
+                <button type="button" class="btn_frmline" onclick="win_zip('fregisterform', 'mb_zip1', 'mb_zip2', 'mb_addr1', 'mb_addr2', 'mb_addr3', 'mb_addr_jibeon');">주소 검색</button>
+                <?php if ($config['cf_req_addr']) { ?><strong class="sound_only">필수</strong><?php } ?>
+                <input type="text" name="mb_addr1" value="<?php echo $member['mb_addr1'] ?>" id="reg_mb_addr1" <?php echo $config['cf_req_addr']?"required":""; ?> class="frm_input frm_address <?php echo $config['cf_req_addr']?"required":""; ?>" placeholder="주소검색을 눌러주세요." size="50"><br>
                 <label for="reg_mb_addr2" class="sound_only">상세주소</label>
-                <input type="text" name="mb_addr2" value="<?php echo $member['mb_addr2'] ?>" id="reg_mb_addr2" class="frm_input frm_address" size="50">
+                <input type="text" name="mb_addr2" value="<?php echo $member['mb_addr2'] ?>" id="reg_mb_addr2" class="frm_input frm_address" placeholder="상세주소" size="50">
                 <br>
                 <label for="reg_mb_addr3" class="sound_only">참고항목</label>
-                <input type="text" name="mb_addr3" value="<?php echo $member['mb_addr3'] ?>" id="reg_mb_addr3" class="frm_input frm_address" size="50" readonly="readonly">
+                <input type="text" name="mb_addr3" value="<?php echo $member['mb_addr3'] ?>" id="reg_mb_addr3" class="frm_input frm_address" placeholder="상세주소 참고항목" size="50" readonly="readonly">
                 <input type="hidden" name="mb_addr_jibeon" value="<?php echo $member['mb_addr_jibeon']; ?>">
             </td>
         </tr>
@@ -146,7 +148,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
         </table>
     </div>
 
-    <div class="tbl_frm01 tbl_wrap">
+    <div class="tbl_frm01 tbl_wrap" style="margin-bottom:12px;">
         <table>
         <caption>VOGOS 정보수신 설정</caption>
         <?php if ($config['cf_use_signature']) { ?>
@@ -184,7 +186,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
         <tr>
             <th scope="row">이메일 수신</th>
             <td>
-                <input type="radio" name="mb_mailling" value="1" id="reg_mb_mailling1" <?php echo ($w=='' || $member['mb_mailling'])?'checked':''; ?>> <label for="reg_mb_mailling1">수신함</label>
+                <input type="radio" name="mb_mailling" value="1" id="reg_mb_mailling1" <?php echo ($w=='' || $member['mb_mailling'])?'checked':''; ?>> <label for="reg_mb_mailling1" style="margin-right:10px;">수신함</label>
                 <input type="radio" name="mb_mailling" value="0" id="reg_mb_mailling0"> <label for="reg_mb_mailling0">수신안함</label>
             </td>
         </tr>
@@ -193,7 +195,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
         <tr>
             <th scope="row"><label for="reg_mb_sms">SMS 수신</label></th>
             <td>
-                <input type="radio" name="mb_sms" value="1" id="reg_mb_sms1" <?php echo ($w=='' || $member['mb_sms'])?'checked':''; ?>> <label for="reg_mb_sms1">수신함</label>
+                <input type="radio" name="mb_sms" value="1" id="reg_mb_sms1" <?php echo ($w=='' || $member['mb_sms'])?'checked':''; ?>> <label for="reg_mb_sms1" style="margin-right:10px;">수신함</label>
                 <input type="radio" name="mb_sms" value="0" id="reg_mb_sms0"> <label for="reg_mb_sms0">수신안함</label>
             </td>
         </tr>
@@ -230,8 +232,8 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
     </div>
 
     <div class="btn_confirm">
-        <input type="submit" value="<?php echo $w==''?'회원가입':'정보수정'; ?>" id="btn_submit" class="btn_submit" accesskey="s">
-        <a href="<?php echo G5_URL; ?>/" class="btn_cancel">취소</a>
+        <!-- <a href="<?php //echo G5_URL; ?>/" class="btn_cancel">취소</a> -->
+        <input type="submit" value="<?php echo $w==''?'가입하기':'정보수정'; ?>" id="btn_submit" class="btn_submit" accesskey="s">
     </div>
     </form>
 
