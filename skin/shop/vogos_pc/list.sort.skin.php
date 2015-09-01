2fbbb3e5b1e837a8aeee733dacee34f290eb8086
Haneul Cho <stuykr@naver.com>
2015-09-01 09:58:28 +0900

    pc 분류 리스트 정렬 코드 최종 완

--- skin/shop/vogos_pc/list.sort.skin.php
+++ skin/shop/vogos_pc/list.sort.skin.php
@@ -10,6 +10,10 @@ if($skin)
     $sct_sort_href .= '&amp;skin='.$skin;
 $sct_sort_href .= '&amp;sort=';
 
+// 상품 정렬 선택시 active 클래스 추가
+$it_order_type = $_GET['sort'];
+$it_order_sc = $_GET['sortodr'];
+
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 add_stylesheet('<link rel="stylesheet" href="'.G5_SHOP_CSS_URL.'/style.css">', 0);
 ?>
@@ -30,12 +34,38 @@ add_stylesheet('<link rel="stylesheet" href="'.G5_SHOP_CSS_URL.'/style.css">', 0
     </ul> -->
 
     <ul id="ssch_sort">
-        <li><a href="<?php echo $sct_sort_href; ?>it_sum_qty&amp;sortodr=desc" class="btn01">판매많은순</a></li>
-        <li><a href="<?php echo $sct_sort_href; ?>it_price&amp;sortodr=asc" class="btn01">낮은가격순</a></li>
-        <li><a href="<?php echo $sct_sort_href; ?>it_price&amp;sortodr=desc" class="btn01">높은가격순</a></li>
-        <!-- <li><a href="<?php //echo $sct_sort_href; ?>it_use_avg&amp;sortodr=desc" class="btn01">평점높은순</a></li> -->
-        <li><a href="<?php echo $sct_sort_href; ?>it_use_cnt&amp;sortodr=desc" class="btn01">후기많은순</a></li>
-        <li><a href="<?php echo $sct_sort_href; ?>it_update_time&amp;sortodr=desc" class="btn01">최근등록순</a></li>
+        <li>
+        <?php if($it_order_type == 'it_price' && $it_order_sc == 'asc') {
+            echo '<a href="'.$sct_sort_href.'it_price&amp;sortodr=asc" class="active btn01">가격 ↓</a>';
+        } else {
+            echo '<a href="'.$sct_sort_href.'it_price&amp;sortodr=asc" class="btn01">가격 ↓</a>';
+        }
+        ?>
+        </li>
+        <li>
+        <?php if($it_order_type == 'it_price' && $it_order_sc == 'desc') {
+            echo '<a href="'.$sct_sort_href.'it_price&amp;sortodr=desc" class="active btn01">가격 ↑</a>';
+        } else {
+            echo '<a href="'.$sct_sort_href.'it_price&amp;sortodr=desc" class="btn01">가격 ↑</a>';
+        }
+        ?>
+        </li>
+        <li>
+        <?php if($it_order_type == 'it_update_time' && $it_order_sc == 'desc') {
+            echo '<a href="'.$sct_sort_href.'it_update_time&amp;sortodr=desc" class="active btn01">최신순</a>';
+        } else {
+            echo '<a href="'.$sct_sort_href.'it_update_time&amp;sortodr=desc" class="btn01">최신순</a>';
+        }
+        ?>
+        </li>
+        <li>
+        <?php if($it_order_type == 'it_order' && $it_order_sc == 'desc') {
+            echo '<a href="'.$sct_sort_href.'it_order&amp;sortodr=desc" class="active btn01">인기순</a>';
+        } else {
+            echo '<a href="'.$sct_sort_href.'it_order&amp;sortodr=desc" class="btn01">인기순</a>';
+        }
+        ?>
+        </li>
     </ul>
 </section>
 <!-- } 상품 정렬 선택 끝 -->
