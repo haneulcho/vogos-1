28850b35ff0eacb03c15ccf93cbab76116af73e0
Haneul Cho <stuykr@naver.com>
2015-09-01 09:16:38 +0900

    pc shop.head.php 전반 수정

--- shop/shop.head.php
+++ shop/shop.head.php
@@ -15,9 +15,6 @@ include_once(G5_LIB_PATH.'/connect.lib.php');
 include_once(G5_LIB_PATH.'/popular.lib.php');
 include_once(G5_LIB_PATH.'/latest.lib.php');
 
-if ($member['mb_level'] < 4 ) { 
-  alert("VOGOS 관리자만 접근하실 수 있습니다. 로그인 하십시오.", G5_BBS_URL.'/login_guest.php?url=' . urlencode(G5_SHOP_URL));
-}
 ?>
 <?php if(defined('_INDEX_')) { ?>
 <!-- 인덱스 슬라이더 owl carousel -->
@@ -34,8 +31,52 @@ if ($member['mb_level'] < 4 ) {
                 <li><a href="#"><img src="<?php echo G5_SHOP_SKIN_URL; ?>/img/sns_ks_s.png" alt="VOGOS 카카오스토리"></a></li>
             </ul>
         </aside>
-        <?php include_once(G5_SHOP_SKIN_PATH.'/boxevent.skin.php'); // 이벤트, 기획전 배너 연동 ?>
         <h1 id="topLogo"><a href="<?php echo $default['de_root_index_use'] ? G5_URL : G5_SHOP_URL; ?>/"><?php echo $config['cf_title']; ?></a></h1>
+        <!-- join, sign in etc... sub nav -->
+        <div id="tnb">
+            <h3>회원메뉴</h3>
+            <ul>
+                <?php if ($is_member) { ?>
+                <?php if ($is_admin) {  ?>
+                <li class="tadmin"><a href="<?php echo G5_ADMIN_URL; ?>/shop_admin/">관리자</a></li>
+                <?php }  ?>
+                <li class="tmypage"><a href="<?php echo G5_SHOP_URL; ?>/cart.php">Cart</a></li>
+                <li class="tmypage"><a href="<?php echo G5_SHOP_URL; ?>/mypage.php">My Page</a></li>
+                <li class="tonlyu"><a href="<?php echo G5_SHOP_URL; ?>/personalpay.php">Only You</a></li>
+                <li class="tsignout"><a href="<?php echo G5_BBS_URL; ?>/logout.php?url=shop">Sign Out</a></li>
+                <?php } else { ?>
+                <li class="tjoin"><a href="<?php echo G5_BBS_URL; ?>/register.php">Join</a></li>
+                <li class="tsignin"><a href="<?php echo G5_BBS_URL; ?>/login.php?url=<?php echo $urlencode; ?>"><b>Sign In</b></a></li>
+                <?php } ?>
+            </ul>
+            <ul>
+                <li class="tfaq"><a href="<?php echo G5_BBS_URL; ?>/faq.php">FAQ</a></li>
+                <li class="tqna"><a href="<?php echo G5_BBS_URL; ?>/qalist.php">Q&amp;A</a></li>
+                <li class="treviews"><a href="<?php echo G5_SHOP_URL; ?>/itemuselist.php">Reviews</a></li>
+                <?php
+                if(!$default['de_root_index_use']) {
+                    $com_href = G5_URL;
+                    $com_name = '커뮤니티';
+
+                    if($default['de_shop_layout_use']) {
+                        if(!preg_match('#'.G5_SHOP_DIR.'/#', $_SERVER['SCRIPT_NAME'])) {
+                            $com_href = G5_SHOP_URL;
+                            $com_name = '쇼핑몰';
+                        }
+                    }
+                ?>
+                <!-- <li><a href="<?php //echo $com_href; ?>/"><?php //echo $com_name; ?></a></li> -->
+                <?php
+                    unset($com_href);
+                    unset($com_name);
+                }
+                ?>
+            </ul>
+        </div>
+        <!-- global navigation -->
+        <?php include_once(G5_SHOP_SKIN_PATH.'/boxcategory.skin.php'); // 상품분류 ?>
+
+        <?php include_once(G5_SHOP_SKIN_PATH.'/boxevent.skin.php'); // 이벤트, 기획전 배너 연동 ?>
         <!-- 검색창 -->
         <div id="hd_sch">
             <h3>쇼핑몰 검색</h3>
@@ -57,84 +98,20 @@ if ($member['mb_level'] < 4 ) {
             }
             </script>
         </div>
-        <!-- join, sign in etc... sub nav -->
-        <div id="tnb">
-            <h3>회원메뉴</h3>
-            <ul>
-                <?php if ($is_member) { ?>
-                <?php if ($is_admin) {  ?>
-                <li class="tadmin"><a href="<?php echo G5_ADMIN_URL; ?>/shop_admin/">Admin</a></li>
-                <?php }  ?>
-                <li class="tmypage"><a href="<?php echo G5_SHOP_URL; ?>/mypage.php">My Page</a></li>
-                <li class="tsignout"><a href="<?php echo G5_BBS_URL; ?>/logout.php?url=shop">Sign Out</a></li>
-                <?php } else { ?>
-                <li class="tjoin"><a href="<?php echo G5_BBS_URL; ?>/register.php">Join</a></li>
-                <li class="tsignin"><a href="<?php echo G5_BBS_URL; ?>/login.php?url=<?php echo $urlencode; ?>"><b>Sign In</b></a></li>
-                <?php } ?>
-            </ul>
-            <ul>
-                <li class="tfaq"><a href="<?php echo G5_BBS_URL; ?>/faq.php">FAQ</a></li>
-                <li class="tqna"><a href="<?php echo G5_BBS_URL; ?>/qalist.php">Q&amp;A</a></li>
-                <li class="tonlyu"><a href="<?php echo G5_SHOP_URL; ?>/personalpay.php">Only You</a></li>
-                <li class="treviews"><a href="<?php echo G5_SHOP_URL; ?>/itemuselist.php">Reviews</a></li>
-                <?php
-                if(!$default['de_root_index_use']) {
-                    $com_href = G5_URL;
-                    $com_name = '커뮤니티';
 
-                    if($default['de_shop_layout_use']) {
-                        if(!preg_match('#'.G5_SHOP_DIR.'/#', $_SERVER['SCRIPT_NAME'])) {
-                            $com_href = G5_SHOP_URL;
-                            $com_name = '쇼핑몰';
-                        }
-                    }
-                ?>
-                <li><a href="<?php //echo $com_href; ?>/"><?php //echo $com_name; ?></a></li>
-                <?php
-                    unset($com_href);
-                    unset($com_name);
-                }
-                ?>
-            </ul>
-        </div>
     </div> <!-- id="header" -->
-    <!-- global navigation -->
-    <?php include_once(G5_SHOP_SKIN_PATH.'/boxcategory.skin.php'); // 상품분류 ?>
 
     <div id="vWrapper"> <!-- contents wrapper -->
     <?php if(defined('_INDEX_')) { ?>
         <div id="mWrapper">
             <div id="index_top" class="w1200">
                 <div id="slider"> <!-- slider contents -->
-                    <?php if($default['de_type4_list_use']) { ?>
-                    <!-- 인기상품 시작 { -->
-                    <section>
-                        <header>
-                            <h2 class="best_item"><a href="<?php echo G5_SHOP_URL; ?>/listtype.php?type=4">BEST ITEMS</a></h2>
-                            <!-- <p class="sct_wrap_hdesc"><?php echo $config['cf_title']; ?> 인기상품 모음</p> -->
-                        </header>
-                        <?php
-                        $list = new item_list();
-                        $list->set_type(4);
-                        $list->set_view('it_id', false);
-                        $list->set_view('it_name', true);
-                        $list->set_view('it_basic', true);
-                        $list->set_view('it_cust_price', true);
-                        $list->set_view('it_price', true);
-                        $list->set_view('it_icon', false);
-                        $list->set_view('sns', false);
-                        echo $list->run();
-                        ?>
-                    </section>
-                    <!-- } 인기상품 끝 -->
-                    <?php } ?>
-
                     <?php if($default['de_type2_list_use']) { ?>
-                    <!-- 추천상품 시작 { -->
+                    <!-- MODEL's CHOICE 시작 { -->
                     <section>
                         <header>
-                            <h2 class="md_choice"><a href="<?php echo G5_SHOP_URL; ?>/listtype.php?type=2">MD's CHOICE</a></h2>
-                            <!-- <p class="sct_wrap_hdesc"><?php echo $config['cf_title']; ?> 추천상품 모음</p> -->
+                            <h2 class="best_item"><a href="<?php echo G5_SHOP_URL; ?>/listtype.php?type=2">MODEL's CHOICE</a></h2>
+                            <!-- <p class="sct_wrap_hdesc"><?php echo $config['cf_title']; ?> MODEL's CHOICE 모음</p> -->
                         </header>
                         <?php
                         $list = new item_list();
@@ -149,14 +126,24 @@ if ($member['mb_level'] < 4 ) {
                         echo $list->run();
                         ?>
                     </section>
-                    <!-- } 추천상품 끝 -->
+                    <!-- } MODEL's CHOICE 끝 -->
+                    <?php } ?>
+
+                    <?php if($default['de_type2_list_use']) { ?>
+                    <!-- NOTICE 시작 { -->
+                    <section>
+                        <header>
+                            <h2 class="md_choice"><a href="<?php echo G5_SHOP_URL; ?>/listtype.php?type=2">NOTICE &amp; EVENT</a></h2>
+                        </header>
+                    </section>
+                    <!-- } NOTICE 끝 -->
                     <?php } ?>
                 </div> <!-- slider end -->
 
                 <!-- 보고스 대표 동영상 시작 { -->
                 <div id="main_video">
                     <?php if($default['de_index_video_use']) { ?>
-                    <?php echo '<iframe src="https://player.vimeo.com/video/'.$default['de_index_video_src'].'?autoplay=1&byline=0" width="'.$default['de_index_video_width'].'" height="'.$default['de_index_video_height'].'" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+                    <?php echo '<iframe src="https://player.vimeo.com/video/'.$default['de_index_video_src'].'?autoplay=0&byline=0" width="'.$default['de_index_video_width'].'" height="'.$default['de_index_video_height'].'" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
                     ?>
                     <?php } ?>
                 </div> <!-- } 보고스 대표 동영상 끝 -->
