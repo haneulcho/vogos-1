b413f621e72a3657d73a99c1d48dd746cb283caf
Haneul Cho <stuykr@naver.com>
2015-09-10 06:08:07 +0900

    pc 게시판 보기 디자인 수정

--- skin/board/basic/list.skin.php
+++ skin/board/basic/list.skin.php
@@ -61,17 +61,17 @@ add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0
         <caption><?php echo $board['bo_subject'] ?> 목록</caption>
         <thead>
         <tr>
-            <th scope="col">번호</th>
+            <th scope="col">Num</th>
             <?php if ($is_checkbox) { ?>
             <th scope="col">
                 <label for="chkall" class="sound_only">현재 페이지 게시물 전체</label>
                 <input type="checkbox" id="chkall" onclick="if (this.checked) all_checked(true); else all_checked(false);">
             </th>
             <?php } ?>
-            <th scope="col">제목</th>
-            <th scope="col">글쓴이</th>
-            <th scope="col"><?php echo subject_sort_link('wr_datetime', $qstr2, 1) ?>날짜</a></th>
-            <th scope="col"><?php echo subject_sort_link('wr_hit', $qstr2, 1) ?>조회</a></th>
+            <th scope="col">Subject</th>
+            <th scope="col">Writer</th>
+            <th scope="col"><?php echo subject_sort_link('wr_datetime', $qstr2, 1) ?>Date</a></th>
+            <th scope="col"><?php echo subject_sort_link('wr_hit', $qstr2, 1) ?>Hit</a></th>
             <?php if ($is_good) { ?><th scope="col"><?php echo subject_sort_link('wr_good', $qstr2, 1) ?>추천</a></th><?php } ?>
             <?php if ($is_nogood) { ?><th scope="col"><?php echo subject_sort_link('wr_nogood', $qstr2, 1) ?>비추천</a></th><?php } ?>
         </tr>
