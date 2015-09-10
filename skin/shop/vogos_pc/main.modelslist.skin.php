904bdf4af914d0117c50acc01b8e1b775ee99863
Haneul Cho <stuykr@naver.com>
2015-09-10 15:54:42 +0900

    pc 인덱스 모델스초이스 css 수정

--- skin/shop/vogos_pc/main.modelslist.skin.php
+++ skin/shop/vogos_pc/main.modelslist.skin.php
@@ -9,13 +9,13 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
     $result = sql_query($sql);
 ?>
 
-<div class="owl-carousel owl30">
+<div class="owl-carousel mdschoice">
 <?php
 for ($i=0; $row=sql_fetch_array($result); $i++) {
 ?>
     <div class="item">
         <div class="slider_img">
-            <a href="<?php echo G5_SHOP_URL.'/models.php?mds_id='.$row['mds_id'] ?>" class="sct_a"><?php echo '<img src="'.G5_DATA_URL.'/models/'.$row['mds_id'].'_b" width="280" height="125">'; ?></a>
+            <a href="<?php echo G5_SHOP_URL.'/models.php?mds_id='.$row['mds_id'] ?>" class="sct_a"><?php echo '<img src="'.G5_DATA_URL.'/models/'.$row['mds_id'].'_b" width="280">'; ?></a>
         </div>
         <div class="slider_txt">
             <a href="<?php echo G5_SHOP_URL.'/models.php?mds_id='.$row['mds_id'] ?>" class="sct_a"><?php echo $row['mds_subject'] ?></a>
@@ -31,7 +31,7 @@ if($i == 0) echo "<p class=\"sct_noitem\">등록된 모델스초이스 상품이
 
 <script>
 $(function() {
-    var owl30 = $('.owl30');
+    var owl30 = $('.mdschoice');
     owl30.owlCarousel({
         items: 1,
         loop: true,
