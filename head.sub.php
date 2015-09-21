c87a95403d573edf93668fe1dc6a4a414ee5033a
Haneul Cho <stuykr@naver.com>
2015-09-21 10:41:25 +0900

    pc 로그분석기 2차 스크립트 추가 삽입

--- head.sub.php
+++ head.sub.php
@@ -93,6 +93,47 @@ if ($is_admin) {
 }
 ?>
 </script>
+<!-- LOG corp Web Analitics & Live Chat START -->
+<script type="text/javascript">
+    /*custom parameters*/
+    var _HCmz={
+        CP:"", //캠페인ID
+        PC:"", //상품명 또는 상품코드
+        PT:"", //카테고리명(카테고리가 여러단계일 경우 ';'으로 구분 [예] AA;BB;CC) 
+        OZ:"", //내부광고/배너분석
+        SO:"", //시나리오(cart:카트,cartend:주소기입,payend:결제완료,REGC:회원가입 또는 약관동의,REGF:입력폼,REGO:회원완료)
+        MP:"", //구매전환상품 (제품코드_가격_제품수량) 여러개 경우 ';'로 구분
+        MA:"", //회원연령(예:39)
+        MS:"", //회원성별(M 또는 W) 
+        IS:"", //내부검색어
+        IC:"", //내부검색어성공여부(성공:Y,실패:N)
+        ES:"", //에러Type
+        MR:"", //회원지역(서울)
+        PS:"", //상품가격(예:29000)
+        PA:"", //장바구니(상품명_수량)  여러개 경우 ';'로 구분 (상품명1_수량;상품명2_수량)
+        OD:""  //주문서코드
+    };
+    function logCorpAScript_full(){
+        HTTP_MSN_MEMBER_NAME="";/*member name*/
+        var prtc=(document.location.protocol=="https:")?"https://":"http://";
+        var hst=prtc+"asp3.http.or.kr";
+        var rnd="r"+(new  Date().getTime()*Math.random()*9);
+        this.ch=function(){
+            if(document.getElementsByTagName("head")[0]){logCorpAnalysis_full.dls();}else{window.setTimeout(logCorpAnalysis_full.ch,30)}
+        }
+        this.dls=function(){
+            var  h=document.getElementsByTagName("head")[0];
+            var  s=document.createElement("script");s.type="text/jav"+"ascript";try{s.defer=true;}catch(e){};try{s.async=true;}catch(e){};
+            if(h){s.src=hst+"/HTTP_MSN/UsrConfig/vogoskorea/js/ASP_Conf.js?s="+rnd;h.appendChild(s);}
+        }
+        this.init= function(){
+            document.write('<img src="'+hst+'/sr.gif?d='+rnd+'"  style="width:1px;height:1px;position:absolute;" alt="" onload="logCorpAnalysis_full.ch()" />');
+        }
+    }
+    if(typeof logCorpAnalysis_full=="undefined"){   var logCorpAnalysis_full=new logCorpAScript_full();logCorpAnalysis_full.init();}
+</script>
+<noscript><img src="http://asp3.http.or.kr/HTTP_MSN/Messenger/Noscript.php?key=vogoskorea" border="0" style="display:none;width:0;height:0;" /></noscript>
+<!-- LOG corp Web Analitics & Live Chat END -->
 <!--  LOG corp Web Analitics & Live Chat  START -->
 <script type="text/javascript">
 //<![CDATA[
