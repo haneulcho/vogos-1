974719dd81f7f66eff8db10c4b601ca95991c2f3
Haneul Cho <stuykr@naver.com>
2015-09-22 20:16:04 +0900

    pc 네이버 프리미엄 로그 분석 버그 수정 (tot_price)

--- shop/cart.php
+++ shop/cart.php
@@ -217,7 +217,7 @@ include_once('./_head.php');
 <!-- 네이버 프리미엄로그분석 전환페이지 설정_ 장바구니담기 -->
  <script type="text/javascript" src="http://wcs.naver.net/wcslog.js"> </script> 
  <script type="text/javascript">
-var $interValue = "<?php echo number_format($tot_price); ?>";
+var $interValue = "<?php echo $tot_price; ?>";
 var _nasa={};
  _nasa["cnv"] = wcs.cnv("3",$interValue);
 </script>
