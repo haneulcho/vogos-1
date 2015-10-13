0239e1f0a09b8da17273f1dbb01527aab036cb16
Haneul Cho <stuykr@naver.com>
2015-10-13 17:18:36 +0900

    pc 커밍순 살리기 *** coming soon

--- index.php
+++ index.php
@@ -1,54 +1,33 @@
-<?php
-define('_INDEX_', true);
-include_once('./_common.php');
-
-// 초기화면 파일 경로 지정 : 이 코드는 가능한 삭제하지 마십시오.
-if ($config['cf_include_index'] && is_file(G5_PATH.'/'.$config['cf_include_index'])) {
-    include_once(G5_PATH.'/'.$config['cf_include_index']);
-    return; // 이 코드의 아래는 실행을 하지 않습니다.
-}
-
-// 루트 index를 쇼핑몰 index 설정했을 때
-if(isset($default['de_root_index_use']) && $default['de_root_index_use']) {
-    require_once(G5_SHOP_PATH.'/index.php');
-    return;
-}
-
-if (G5_IS_MOBILE) {
-    include_once(G5_MOBILE_PATH.'/index.php');
-    return;
-}
-
-include_once('./_head.php');
-?>
-
-<h2 class="sound_only">최신글</h2>
-<!-- 최신글 시작 { -->
-<?php
-//  최신글
-$sql = " select bo_table
-            from `{$g5['board_table']}` a left join `{$g5['group_table']}` b on (a.gr_id=b.gr_id)
-            where a.bo_device <> 'mobile' ";
-if(!$is_admin)
-    $sql .= " and a.bo_use_cert = '' ";
-$sql .= " order by b.gr_order, a.bo_order ";
-$result = sql_query($sql);
-for ($i=0; $row=sql_fetch_array($result); $i++) {
-    if ($i%2==1) $lt_style = "margin-left:20px";
-    else $lt_style = "";
-?>
-    <div style="float:left;<?php echo $lt_style ?>">
-        <?php
-        // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
-        // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
-        echo latest("basic", $row['bo_table'], 5, 25);
-        ?>
+<!doctype html>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="imagetoolbar" content="no">
+<meta http-equiv="X-UA-Compatible" content="IE=10,chrome=1">
+<title>VOGOS</title>
+<link rel="stylesheet" href="http://vogos.com/css/common.css">
+<!--[if lte IE 8]>
+<script src="http://vogos.com/js/html5.js"></script>
+<![endif]-->
+<script>
+// 자바스크립트에서 사용하는 전역변수 선언
+var g5_url       = "http://vogos.com";
+var g5_bbs_url   = "http://vogos.com/bbs";
+var g5_is_member = "1";
+var g5_is_admin  = "super";
+var g5_is_mobile = "";
+var g5_bo_table  = "";
+var g5_sca       = "";
+var g5_editor    = "";
+var g5_cookie_domain = "";
+var g5_admin_url = "http://vogos.com/adm";
+</script>
+</head>
+<body>
+<div id="wrapper">
+    <div class="coming">
+        <img src="comingsoon.png">
     </div>
-<?php
-}
-?>
-<!-- } 최신글 끝 -->
-
-<?php
-include_once('./_tail.php');
-?>
+</div>
+</body>
+</html>
--- lib/shop.lib.php
+++ lib/shop.lib.php
@@ -60,7 +60,8 @@ class item_list
     protected $ca_id3 = "";
 
     // 노출순서
-    protected $order_by = "it_order, it_time, it_id desc";
+    // protected $order_by = "it_order, it_update_time, it_id desc";
+    protected $order_by = "it_order, it_update_time desc";
 
     // 상품의 이벤트번호를 저장합니다.
     protected $event = "";
