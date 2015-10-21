4a740988a574bbecdb985f48424d3256bf4ff9cd
Haneul Cho <stuykr@naver.com>
2015-10-21 12:08:48 +0900

    pc 상품리스트 버그 수정

--- skin/shop/vogos_pc/list.20.skin.php
+++ skin/shop/vogos_pc/list.20.skin.php
@@ -55,7 +55,7 @@ for ($i=0; $row=sql_fetch_array($result); $i++) {
     }
 
     if ($this->view_it_img) {
-        echo get_it_image($row['it_id'], $this->img_width, $this->img_height, '', '', stripslashes($row['it_name']))."\n";
+        echo get_it_image_best($row['it_id'], $this->img_width, $this->img_height, 8, '', '', 'original', stripslashes($row['it_name']))."\n";
     }
 
     if ($this->href) {
