b77d14180bed3012f31f54a2a9f0544b23ec83f1
Haneul Cho <stuykr@naver.com>
2015-11-25 19:05:55 +0900

    pc 블랙 프라이데이(=black friday) 이벤트 삽입, 에디터스픽 추가

--- skin/shop/vogos_pc/main.10.skin.php
+++ skin/shop/vogos_pc/main.10.skin.php
@@ -2,16 +2,16 @@
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
-add_stylesheet('<link rel="stylesheet" href="'.G5_SHOP_SKIN_URL.'/style.css">', 0);
+add_stylesheet('<link rel="stylesheet" href="'.G5_SHOP_CSS_URL.'/style.css">', 0);
 add_javascript('<script src="'.G5_SHOP_SKIN_URL.'/js/jquery.shop.list.js"></script>', 10);
 ?>
-
-<!-- 상품진열 10 시작 { -->
+ 
+<!-- 상품진열 20 (분류 기본 리스트) 시작 { -->
 <?php
 for ($i=0; $row=sql_fetch_array($result); $i++) {
-    if ($i % $this->list_mod == 0)
+    if ($i % $this->list_mod == 0) { // 1줄 이미지 : 2개 이상
         $sct_last = ' sct_clear'; // 줄 첫번째
-    else {
+    } else { // 1줄 이미지 : 1개
         $sct_last = '';
     }
 
@@ -19,7 +19,7 @@ for ($i=0; $row=sql_fetch_array($result); $i++) {
         if ($this->css) {
             echo "<ul class=\"{$this->css}\">\n";
         } else {
-            echo "<ul class=\"sct sct_10\">\n";
+            echo "<ul class=\"sct sch_list\">\n";
         }
     }
 
@@ -30,16 +30,41 @@ for ($i=0; $row=sql_fetch_array($result); $i++) {
     }
 
     if ($this->view_it_img) {
-        echo get_it_image($row['it_id'], $this->img_width, $this->img_height, '', '', stripslashes($row['it_name']))."\n";
+        echo get_it_image_best($row['it_id'], $this->img_width, $this->img_height, 8, '', '', 'original', stripslashes($row['it_name']))."\n";
+    }
+
+    if ($this->href) {
+        echo "<div class=\"sct_des\"><div class=\"sct_txt_big\"><a href=\"{$this->href}{$row['it_id']}\" class=\"sct_a\">\n";
+    }
+
+    if ($this->view_it_name) {
+        echo stripslashes($row['it_name'])."\n";
+        echo "</a>\n";
     }
 
     if ($this->href) {
         echo "</a></div>\n";
     }
 
+    if ($this->view_it_basic && $row['it_basic']) {
+        echo "<div class=\"sct_basic\">".stripslashes($row['it_basic'])."</div>\n";
+    }
 
     if ($this->href) {
-        echo "<div class=\"sct_txt\"><a href=\"{$this->href}{$row['it_id']}\" class=\"sct_a\">\n";
+        echo "<a class=\"sct_buynow\" href=\"{$this->href}{$row['it_id']}\" class=\"sct_a\">BUY NOW</a>\n";
+    }
+
+    if ($this->href) {
+        echo "</div>\n"; //sct_des END
+    }
+
+    if ($this->href) {
+        echo "</div>\n";
+    }
+
+
+    if ($this->href) {
+        echo "<div class=\"sct_des_bottom\"><div class=\"sct_txt\"><a href=\"{$this->href}{$row['it_id']}\" class=\"sct_a\">\n";
     }
 
     if ($this->view_it_name) {
@@ -64,21 +89,44 @@ for ($i=0; $row=sql_fetch_array($result); $i++) {
 
         echo "</div>\n";
 
-        echo"<div class=\"sct_cart\">
-            <div class=\"sct_cart_btn\"  style=\"width:{$this->img_width}px;height:{$this->img_height}px\">
-                <button type=\"button\" class=\"btn_wish\" data-it_id=\"{$row['it_id']}\">위시</button>
-                <button type=\"button\" class=\"btn_cart\" data-it_id=\"{$row['it_id']}\">장바구니</button>
+        echo"<div class=\"sct_cart_m\">
+            <div class=\"sct_cart_btn\" style=\"width:70px;height:40px\">
+                <button type=\"button\" class=\"btn_cart_m\" data-it_id=\"{$row['it_id']}\"><i class=\"ion-android-cart\"></i>CART</button>
             </div>
-            <div class=\"sct_cart_op\"  style=\"width:{$this->img_width}px;height:{$this->img_height}px\"></div>
+            <div class=\"sct_cart_op\"></div>
         </div>\n";
 
+        echo "</div>\n";
 
-}
+    }
+
     echo "</li>\n";
 }
 
 if ($i > 0) echo "</ul>\n";
 
-if($i == 0) echo "<p class=\"sct_noitem\">등록된 상품이 없습니다.</p>\n";
+if($i == 0) echo "<p class=\"sct_noitem\">Your search returns no results.</p>\n";
 ?>
-<!-- } 상품진열 10 끝 -->
+<script>
+  jQuery(function($){ 
+    $(".sct li:nth-child(odd)").addClass("odd_margin");
+  }); 
+</script>
+
+<script type="text/javascript">
+$(function() {
+    var $li50 = $('.sch_list li .sct_img');
+    $li50.each(function() {
+        var $des = $(this).children('.sct_des');
+        $des.hide();
+        $(this).mouseenter(function(e) {
+            $des.filter(':not(:animated)').fadeIn(400);
+        })
+        .mouseleave(function() {
+            $('.sch_list li .sct_des').hide();
+            $des.filter(':not(:animated)').fadeOut(400);
+        });
+    });
+});
+</script>
+<!-- } 상품진열 20 끝 -->Z
